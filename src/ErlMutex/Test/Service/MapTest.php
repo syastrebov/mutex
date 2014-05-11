f677b174fcb7480b2f9a5c73652ea3009ecc4f2d
syastrebov <serg.yastrebov@gmail.com>
2014-05-11 20:51:56 +0400

    unique map

---
+++ src/ErlMutex/Test/Service/MapTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * PHP-Erlang erl
+ * Сервис блокировок для обработки критических секций
+ *
+ * @category erl
+ * @package  erl
+ * @author   Sergey Yastrebov <serg.yastrebov@gmail.com>
+ * @link     https://github.com/syastrebov/erl
+ */
+
+namespace ErlMutex\Test\Service;
+
+/**
+ * Class MapTest
+ * @package ErlMutex\Test\Service
+ */
+class MapTest  extends \PHPUnit_Framework_TestCase
+{
+
+}
