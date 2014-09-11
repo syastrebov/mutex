bf2b3b79f189c7399c7aaf2387090c559ce13c20
Sergey Yastrebov <djay@djay-K56CB.(none)>
2014-09-11 19:11:10 +0400

    adapters

---
+++ src/ErlMutex/Adapter/AdapterInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * PHP-Erlang erl
+ * Сервис блокировок для обработки критических секций
+ *
+ * @category erl
+ * @package  erl
+ * @author   Sergey Yastrebov <serg.yastrebov@gmail.com>
+ * @link     https://github.com/syastrebov/mutex-php
+ */
+
+namespace ErlMutex\Adapter;
+
+use ErlMutex\Exception\Exception;
+
+/**
+ * Интерфейс адаптера
+ *
+ * Class AdapterInterface
+ * @package ErlMutex\Adapter
+ */
+interface AdapterInterface
+{
+    /**
+     * Получить указатель на блокировку
+     *
+     * @param string   $name    Имя указателя блокировки
+     * @param int|bool $timeout Время жизни блокировки, микросекунды (по истечении времени блокировка снимается)
+     *
+     * @return string
+     * @throws Exception
+     */
+    public function get($name, $timeout=false);
+
+    /**
+     * Установить блокировку
+     *
+     * @param string $name Имя указателя блокировки
+     * @return bool
+     */
+    public function acquire($name=null);
+
+    /**
+     * Снять блокировку
+     *
+     * @param string $name Имя указателя блокировки
+     * @return bool
+     */
+    public function release($name=null);
+}
--- src/ErlMutex/Adapter/Socket.php
+++ src/ErlMutex/Adapter/Socket.php
@@ -12,13 +12,66 @@
 
 namespace ErlMutex\Adapter;
 
+use ErlMutex\Exception\Exception;
+use ErlMutex\LoggerInterface;
+use ErlMutex\Service\Profiler;
+
 /**
  * Адаптер для работы через socket
  *
  * Class Socket
  * @package ErlMutex\Adapter
  */
-class Socket
+class Socket implements AdapterInterface
 {
+    const DEFAULT_HOST   = '127.0.0.1';
+    const DEFAULT_PORT   = 7007;
 
+    const ACTION_GET     = 'get';
+    const ACTION_ACQUIRE = 'acquire';
+    const ACTION_RELEASE = 'release';
+
+    /**
+     * @var string
+     */
+    private $hostname;
+
+    /**
+     * @var int
+     */
+    private $port;
+
+    /**
+     * @var resource
+     */
+    private $socket;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var Profiler
+     */
+    private $profiler;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor
+     *
+     * @param string $hostname Хост сервиса блокировок
+     * @param int    $port     Порт сервиса блокировок (по умолчанию 7007)
+     *
+     * @throws Exception
+     */
+    public function __construct($hostname=self::DEFAULT_HOST, $port=self::DEFAULT_PORT)
+    {
+        $this->hostname = $hostname;
+        $this->port     = $port;
+    }
 }
