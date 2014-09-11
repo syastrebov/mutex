4bb349f510cb5e94f8f1e0fd9f04782c84f751cf
Sergey Yastrebov <djay@djay-K56CB.(none)>
2014-09-11 18:52:39 +0400

    adapters

---
+++ src/ErlMutex/Adapter/Memcached.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * PHP-Erlang erl
+ * Сервис блокировок для обработки критических секций
+ *
+ * @category erl
+ * @package  erl
+ * @author   Sergey Yastrebov <serg.yastrebov@gmail.com>
+ * @link     https://github.com/syastrebov/mutex-php
+ */
+
+namespace ErlMutex\Adapter;
+
+/**
+ * Адаптер для работы через memcached
+ *
+ * Class Memcached
+ * @package ErlMutex\Adapter
+ */
+class Memcached
+{
+
+}
